0fedb68e9b6bf08d6d6cad843e8c3d8de07a9f93
gugu <Exaze Trsa25@EXZ_MP2HMR98>
2025-07-11 08:15:46 +0200

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    public sealed class Logger // seal class, to restrict all th inharitance
+    {
+
+        private static Logger _instance;
+        private static readonly object InstanceLock = new object();
+
+        private Logger() // Private constructor
+        {
+            Console.WriteLine("Logger constructor executed\n");
+        }
+
+        public static Logger Instance // Singleton instance accessor
+        {
+            get
+            {
+                lock (InstanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                    return _instance;
+                }
+            }
+        }
+        public void Log(string message) // Instance method to log messages
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger s1 = Logger.Instance;
+        Logger s2 = Logger.Instance;
+
+        s1.Log("First log message.");
+        s2.Log("Second log message.");
+
+        Console.WriteLine(s1 == s2);
+    }
+}
